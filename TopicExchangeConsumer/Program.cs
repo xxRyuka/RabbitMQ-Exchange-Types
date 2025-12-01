6b567445540f9d64b3bb00d14d3662f9d9c844e6
xxRyuka <enesfurkankus16@gmail.com>
2025-12-02 01:56:59 +0300

    Topic Exchange implementation

---
+++ TopicExchangeConsumer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+// Uygulama başlarken hangi konuyu dinlemek istediğimizi soralım
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Kullanım: dotnet run -- <binding_key>");
+    Console.Error.WriteLine("Örnek: dotnet run -- backend.#");
+    Environment.ExitCode = 1;
+    return;
+}
+var lsit = args;
+
+foreach (var arg in args)
+{
+    Console.WriteLine(arg);
+}
+// Komut satırından gelen desen (* veya # içerebilir)
+var bindingKey = args[0];
+
+ConnectionFactory factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest",
+};
+var connectionAsync = await factory.CreateConnectionAsync();
+
+
+var ch = await connectionAsync.CreateChannelAsync();
+
+//(Publisher ile aynı olmalı)
+// Consumer publisherdan erken calısırsa hata almamak için exchangeyi burdada tanımlıyoruz 
+await ch.ExchangeDeclareAsync(
+    exchange: "topic-logs",
+    durable: true,
+    autoDelete: false,
+    arguments: null,
+    type: ExchangeType.Topic
+);
+
+
+var queueDeclareOk = await ch.QueueDeclareAsync();
+var queueName = queueDeclareOk.QueueName;
+
+
+// 3. KRİTİK NOKTA: Kuyruğu Exchange'e bağla (Binding)
+// Burada Exchange'e diyoruz ki: "Şu desene uyan mesajları benim kuyruğuma yönlendir."
+await ch.QueueBindAsync(queue: queueName,
+    exchange: "topic-logs",
+    routingKey: bindingKey);
+
+Console.WriteLine($"[*] '{bindingKey}' deseni ile dinleniyor. Mesajlar bekleniyor...");
+
+var consumer = new AsyncEventingBasicConsumer(ch);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine($" [x] ALINDI | Key: {routingKey} | Msg: {message}");
+
+    await ch.BasicAckAsync(
+        deliveryTag: ea.DeliveryTag,
+        multiple: false);
+};
+
+await ch.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Çıkış için [enter] tuşuna basın.");
+Console.ReadLine();
---
+++ TopicExchangePublisher/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest",
+};
+var connectionAsync = await factory.CreateConnectionAsync();
+
+
+var ch = await connectionAsync.CreateChannelAsync();
+
+await ch.ExchangeDeclareAsync(
+    exchange: "topic-logs",
+    durable: true,
+    autoDelete: false,
+    arguments: null,
+    type: ExchangeType.Topic
+);
+
+Console.WriteLine("Log göndermek için format: <routingKey> <mesaj>");
+Console.WriteLine("Örnek: 'backend.error.payment Ödeme servisi çöktü'");
+Console.WriteLine("Çıkış için 'exit' yazın.\n");
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == "exit") break;
+
+    // Girdiyi RoutingKey ve Mesaj olarak ayır
+    var parts = input!.Split(' ', 2);
+
+    var routingKey = parts[0];
+    var message = parts[1];
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    BasicProperties properties = new BasicProperties();
+    properties.Persistent = true;
+    // Mesajı Exchange'e gönder (Routing Key burada kritik)
+    await ch.BasicPublishAsync(
+        exchange: "topic-logs",
+        routingKey: routingKey,
+        basicProperties: properties,
+        mandatory: true,
+        body: body
+    );
+
+    Console.WriteLine($"[x] Gönderildi -> Key: '{routingKey}', Mesaj: '{message}'");
+
+
+}
+    ch.BasicReturnAsync += async (model, ea) =>
+    {
+        var replyText = ea.ReplyText;
+        var replyCode = ea.ReplyCode;
+        var routingKey = ea.RoutingKey;
+        var msg = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+
+        Console.WriteLine("Message Returned");
+        Console.WriteLine($"Props \n" +
+                          $"msg: {msg} \n" +
+                          $"replyText : {replyText} \n" +
+                          $"replyCode : {replyCode} \n" +
+                          $"routingKey : {routingKey} \n");
+    };
